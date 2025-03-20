e969ab5d4355b7afc69214164deeb73da010edcf
kitik <svy1dan@gmail.com>
2025-03-20 16:00:11 +0300

    equip add component

---
+++ components/equip add component/EquipAdd.jsx
@@ -0,0 +1,62 @@
+import './ecuipAdd.css';
+
+export default function equipAdd() {
+    return (
+        <>
+            <div className="equipAddMain">
+                <div className="equipAddContainer">
+                    <div className="equipAdd">
+                        <button>
+                            <img src={"https://cdn-icons-png.flaticon.com/512/25/25333.png"}/>
+                        </button>
+                    </div>
+                    <div className="equipAdd">
+                        <button>
+                            <img src={"https://cdn-icons-png.flaticon.com/512/25/25333.png"}/>
+                        </button>
+                    </div>
+                    <div className="equipAdd">
+                        <button>
+                            <img src={"https://cdn-icons-png.flaticon.com/512/25/25333.png"}/>
+                        </button>
+                    </div>
+                    <div className="equipAdd">
+                        <button>
+                            <img src={"https://cdn-icons-png.flaticon.com/512/25/25333.png"}/>
+                        </button>
+                    </div>
+                    <div className="equipAdd">
+                        <button>
+                            <img src={"https://cdn-icons-png.flaticon.com/512/25/25333.png"}/>
+                        </button>
+                    </div>
+                    <div className="equipAdd">
+                        <button>
+                            <img src={"https://cdn-icons-png.flaticon.com/512/25/25333.png"}/>
+                        </button>
+                    </div>
+                    <div className="equipAdd">
+                        <button>
+                            <img src={"https://cdn-icons-png.flaticon.com/512/25/25333.png"}/>
+                        </button>
+                    </div>
+                    <div className="equipAdd">
+                        <button>
+                            <img src={"https://cdn-icons-png.flaticon.com/512/25/25333.png"}/>
+                        </button>
+                    </div>
+                    <div className="equipAdd">
+                        <button>
+                            <img src={"https://cdn-icons-png.flaticon.com/512/25/25333.png"}/>
+                        </button>
+                    </div>
+                    <div className="equipAdd">
+                        <button>
+                            <img src={"https://cdn-icons-png.flaticon.com/512/25/25333.png"}/>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
