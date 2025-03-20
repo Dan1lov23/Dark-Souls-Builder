d0006aa28806447a52d522ea4e172cff494b6d44
kitik <svy1dan@gmail.com>
2025-03-20 15:59:06 +0300

    add changes

--- pages/Home page/Home.jsx
+++ pages/Home page/Home.jsx
@@ -1,9 +1,11 @@
 import CharacterRedactor from '../../components/character redactor/Redactor.jsx'
+import EqiupAdd from '../../components/equip add component/EquipAdd.jsx';
 
 export default function Home() {
     return (
         <>
             <CharacterRedactor />
+            <EqiupAdd />
         </>
     )
 }
