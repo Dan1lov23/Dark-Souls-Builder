254594d1e1cd47b99408e67de05a4633a0551950
kitik <svy1dan@gmail.com>
2025-03-21 11:48:51 +0300

    add delete equip function

--- pages/Home page/Home.jsx
+++ pages/Home page/Home.jsx
@@ -1,11 +1,24 @@
-import CharacterRedactor from '../../components/character redactor/Redactor.jsx'
-import EqiupAdd from '../../components/equip add component/EquipAdd.jsx';
+import CharacterRedactor from '../../components/character redactor/Redactor.jsx';
+import EquipAdd from '../../components/equip add component/EquipAdd.jsx';
+import EquipList from "../equip list page/equipList.jsx";
+import { useState } from "react";
 
 export default function Home() {
+    const [userItem, setUserItem] = useState([]); // Инициализация состояния userItem как пустого массива
+
+    // Функция для удаления элемента
+    const removeItem = (index) => {
+        setUserItem(prevItems => prevItems.filter((_, i) => i !== index));
+    };
+
     return (
         <>
             <CharacterRedactor />
-            <EqiupAdd />
+            <EquipList userItem={userItem} setUserItem={setUserItem} />
+            <EquipAdd userItem={userItem} removeItem={(index) => {
+                const newItems = userItem.filter((_, i) => i !== index);
+                setUserItem(newItems);
+            }} />
         </>
-    )
+    );
 }
