979e551fad8181f7c5529c15d681a684859fad9c
kitik <svy1dan@gmail.com>
2025-03-21 11:46:09 +0300

    add many changes and add new weapons type

--- pages/equip list page/equipList.jsx
+++ pages/equip list page/equipList.jsx
@@ -1,6 +1,15 @@
 import './equipList.css';
 
-export default function EquipList() {
+export default function EquipList({ userItem, setUserItem }) {
+
+
+    function addItem(src, name) {
+        if (userItem.length < 2) {
+            const newItem = { src, name };
+            setUserItem([...userItem, newItem]);
+        }
+    }
+
 
     const weaponsTypes = [
         {
@@ -24,19 +33,43 @@ export default function EquipList() {
                 {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/broadsword.png", name: "Broadsword"},
                 {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/shortsword.png", name: "Shortsword"},
                 {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/Longsword.png", name: "Longsword"},
-                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/barbed_straight_sword.png", name: "Barbed Straight"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/barbed_straight_sword.png", name: "Barbed Straight Sword"},
                 {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/sunlight_straight_sword.png", name: "Sunlight Straight sword"},
-                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/balder_side_sword.png", name: "Balder Side sword"},
-                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/astoras_straight_sword.png", name: "Astoras Straight Sword"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/balder_side_sword.png", name: "Balder Side Sword"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/astoras_straight_sword.png", name: "Astora's Straight Sword"},
                 {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/silv_knight_str_sword.png", name: "Silver Knight Straight Sword"},
                 {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/Drake_Sword.png", name: "Drake Sword"},
                 {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/Darksword.png", name: "Darksword"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/crystal_straight_sword.png", name: "Crystal Straight Sword"},
+            ]
+        },
+        {
+            name: "Greatswords",
+            weapons: [
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/bastard_sword.png", name: "Bastard Sword"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/black_knight_sword.png", name: "Black Knight Sword"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/claymore.png", name: "Claymore"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/flamberge.png", name: "Flamberge"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/crystal_greatsword_1.png", name: "Crystal Greatsword"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/manserpent_greatsword.png", name: "Man-Serpent Greatsword"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/great_lord_greatsword_1.png", name: "Great Lord Greatsword"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/moonlight_greatsword.png", name: "Moonlight Greatsword"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/greatsword_of_artorias.png", name: "greatsword of artorias"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/greatsword_of_artorias_3.png", name: "Greatsword of Artorias (Cursed)"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/stone_greatsword.png", name: "stone greatsword"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/Abyss%20Greatsword.png", name: "Abyss Greatsword"},
+                {src: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/Obsidian%20Greatsword.png", name: "Obsidian Greatsword"},
+
+
             ]
         }
     ];
 
     return (
         <>
+            <div className="equipListMainTitle">
+                <h1>Dark Souls weapons</h1>
+            </div>
             <div className="equipListMain">
                 {weaponsTypes.map((weaponType, index) => (
                     <div key={index}>
@@ -45,12 +78,12 @@ export default function EquipList() {
                         </div>
                         <div className="equipListContainer">
                             {weaponType.weapons.map((weapon, weaponIndex) => (
-                                <div className="weaponMain" key={weaponIndex}>
-                                    <img src={weapon.src} alt={weapon.name} />
-                                    <p>______________</p>
-                                    <a href={`https://darksouls.wiki.fextralife.com/${weapon.name}`}>
-                                        <span>{weapon.name}</span>
-                                    </a>
+                                <div className="weaponMain" key={weaponIndex} onClick={() => addItem(weapon.src, weapon.name)}>
+                                        <img src={weapon.src} alt={weapon.name} />
+                                        <p>______________</p>
+                                        <a href={`https://darksouls.wiki.fextralife.com/${weapon.name}`}>
+                                            <span>{weapon.name}</span>
+                                        </a>
                                 </div>
                             ))}
                         </div>
